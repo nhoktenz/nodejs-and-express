779df007a3ad77f60f9285b3b8760a8fc3886e7c
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-04-14 13:50:43 -0400

    change sessions

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,22 +1,40 @@
-import express from 'express';
-import sessions from '../data/sessions.json' assert {type: 'json'};
+import express from "express";
+import sessions from "../data/sessions.json" assert { type: "json" };
+import { MongoClient } from "mongodb";
+import Debug from "debug";
 
+const debug = Debug("app");
+const debug2 = Debug("app:sessionsRouter");
 
 const sessionRouter = express.Router();
 
-sessionRouter.route('/')
-.get((req,res) => {
-    res.render('sessions',{
+sessionRouter.route("/").get((req, res) => {
+  const url =
+    "mongodb+srv://tenz:x1ozJ86cGubTqbm6@globomatics.0aqb1.mongodb.net/globomantics?retryWrites=true&w=majority";
+  const dbName = "globomantics";
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connected to the mongo DB");
+
+      const db = client.db(dbName);
+
+      const sessions = await db.collection("sessions").find().toArray();
+
+      res.render("sessions", {
         sessions,
-    });
+      });
+    } catch (error) {
+      debug(error.stack);
+    }
+  })();
 });
-sessionRouter.route('/:id')
-.get((req,res)=>{
-    const id = req.params.id;
-    res.render('session', 
-    {
-        session: sessions[id]
-    });
+sessionRouter.route("/:id").get((req, res) => {
+  const id = req.params.id;
+  res.render("session", {
+    session: sessions[id],
+  });
 });
 
 export default sessionRouter;
