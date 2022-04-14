da4e462f19d1c34682ab7091440a38ec32093429
Thuong Nguyen <nhoktenz.tech@gmail.com>
2022-04-14 13:56:01 -0400

    edit single session

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import sessions from "../data/sessions.json" assert { type: "json" };
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import Debug from "debug";
 
 const debug = Debug("app");
@@ -30,11 +30,32 @@ sessionRouter.route("/").get((req, res) => {
     }
   })();
 });
+
 sessionRouter.route("/:id").get((req, res) => {
   const id = req.params.id;
-  res.render("session", {
-    session: sessions[id],
-  });
+
+  const url =
+    "mongodb+srv://tenz:x1ozJ86cGubTqbm6@globomatics.0aqb1.mongodb.net/globomantics?retryWrites=true&w=majority";
+  const dbName = "globomantics";
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connected to the mongo DB");
+
+      const db = client.db(dbName);
+
+      const session = await db
+        .collection("sessions")
+        .findOne({ _id: new ObjectId(id) });
+
+      res.render("session", {
+        session,
+      });
+    } catch (error) {
+      debug(error.stack);
+    }
+  })();
 });
 
 export default sessionRouter;
